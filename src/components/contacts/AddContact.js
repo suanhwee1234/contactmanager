d35268417330aa077eac72e7b9612d302aebb198
suanhwee <suanhwee@gmail.com>
2021-04-20 00:32:28 +0800

    test

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,69 +1,96 @@
-import React, { Component } from 'react'
-import { Consumer } from '../../context'
-import TextInputGroup from '../layout/TextInputGroup'
-import { v4 as uuid } from 'uuid';
+import React, { Component } from "react";
+import { Consumer } from "../../context";
+import TextInputGroup from "../layout/TextInputGroup";
+import { v4 as uuid } from "uuid";
 
 class AddContact extends Component {
-    state = {
-        name: '',
-        email: '',
-        phone: '',
-        errors:{},
+  state = {
+    name: "",
+    email: "",
+    phone: "",
+    errors: {},
+  };
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  onSubmit = (dispatch, e) => {
+    e.preventDefault();
+    const { name, email, phone } = this.state;
+    if (name === "") {
+      this.setState({ errors: { name: "Name is required" } });
+      return;
     }
-    onChange = e => this.setState({ [e.target.name]: e.target.value })
-    onSubmit = (dispatch, e) => {
-        e.preventDefault();
-        const { name, email, phone } = this.state;
-        if(name===''){
-            this.setState({errors:{name:'Name is required'}});
-            return;
-        }
-        if(email===''){
-            this.setState({errors:{email:'email is required'}});
-            return;
-        }
-        if(phone===''){
-            this.setState({errors:{phone:'phone is required'}});
-            return;
-        }
-
-        const newContact = {
-            id: uuid(),
-            name, email, phone
-        }
-        dispatch({ type: 'ADD_CONTACT', payload: newContact })
-        this.setState({
-            name: '',
-            email: '',
-            phone: '',
-            errors:{},
-        })
-
+    if (email === "") {
+      this.setState({ errors: { email: "email is required" } });
+      return;
     }
-    render() {
-        const { name, email, phone,errors } = this.state;
-        return (<Consumer>
-            {value => {
-                const { dispatch } = value;
-                return (
-                    <div className="card mb-3">
-                        <div className="card-header">Add Contact</div>
-                        <div className="card-body">
-                            <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                                <TextInputGroup label="Name" name="name" placeholder="Enter Name" value={name} onChange={this.onChange} errors={errors.name} />
-                                <TextInputGroup label="Email" name="email" type="email" placeholder="Enter Email..." value={email} onChange={this.onChange} errors={errors.email}/>
-                                <TextInputGroup label="phone" name="phone" placeholder="Enter phone" value={phone} onChange={this.onChange} errors={errors.phone} />
-
-                                <input type="submit" className="btn btn-light btn-block" value="Add Contact" />
-                            </form>
-                        </div>
-
-                    </div>
-                )
-            }}
-        </Consumer>)
-
-
+    if (phone === "") {
+      this.setState({ errors: { phone: "phone is required" } });
+      return;
     }
+
+    const newContact = {
+      id: uuid(),
+      name,
+      email,
+      phone,
+    };
+    dispatch({ type: "ADD_CONTACT", payload: newContact });
+    this.setState({
+      name: "",
+      email: "",
+      phone: "",
+      errors: {},
+    });
+    this.props.history.push("/");
+  };
+  render() {
+    const { name, email, phone, errors } = this.state;
+    return (
+      <Consumer>
+        {(value) => {
+          const { dispatch } = value;
+          return (
+            <div className="card mb-3">
+              <div className="card-header">Add Contact</div>
+              <div className="card-body">
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter Name"
+                    value={name}
+                    onChange={this.onChange}
+                    errors={errors.name}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    type="email"
+                    placeholder="Enter Email..."
+                    value={email}
+                    onChange={this.onChange}
+                    errors={errors.email}
+                  />
+                  <TextInputGroup
+                    label="phone"
+                    name="phone"
+                    placeholder="Enter phone"
+                    value={phone}
+                    onChange={this.onChange}
+                    errors={errors.phone}
+                  />
+
+                  <input
+                    type="submit"
+                    className="btn btn-light btn-block"
+                    value="Add Contact"
+                  />
+                </form>
+              </div>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
 }
 export default AddContact;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,41 +1,51 @@
-import React from 'react';
-import PropTypes from "prop-types"
-const Header= (props)=>{
-    const {branding}=props;
-    return (
-        <div>
-          <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
-              <div className="container">
-                  <a href="/" className="navbar-brand">{branding}</a>
-              </div>
-                <div>
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a href="/" className="nav-link">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/" className="nav-link">Home</a>
-                        </li>
-
-                    </ul>
-                </div>
-          </nav>
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+const Header = (props) => {
+  const { branding } = props;
+  return (
+    <div>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+        <div className="container">
+          <a href="/" className="navbar-brand">
+            {branding}
+          </a>
         </div>
-    )
-}
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                <i className="fas fa-home">Home</i>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact/add" className="nav-link">
+                <i className="fas fa-plus">Add</i>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                <i className="fas fa-question">About</i>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-Header.defaultProps={
-    branding:'My App',
-}
+Header.defaultProps = {
+  branding: "My App",
+};
 
-Header.propTypes={
-    branding:PropTypes.string.isRequired
-}
+Header.propTypes = {
+  branding: PropTypes.string.isRequired,
+};
 
-const headingStyle={
-    color:'green',
-    fontSize:'50px',
-}
+const headingStyle = {
+  color: "green",
+  fontSize: "50px",
+};
 
 export default Header;
