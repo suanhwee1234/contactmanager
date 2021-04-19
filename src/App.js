d630b8ccc979c29e6d979575814d0e9daf370c16
suanhwee1234 <suanhwee@gmail.com>
2021-04-19 22:07:36 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css"
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
+import About from './components/pages/about'
 import Header from './components/layout/Header'
 import { Provider } from './context';
 
 function App() {
   return (
     <Provider>
+      <Router>
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-          <AddContact />
-          <Contacts />
+          <Switch>
+            <Route exact path="/" component={Contacts}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/contact/add" component={AddContact}/>
+          </Switch>
+          
 
         </div>
       </div>
+      </Router>
     </Provider>
   );
 }
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -13,12 +13,15 @@ class Contacts extends Component {
                     return (
 
                         <React.Fragment>
+                            <h1 className="display-4 mb-2">
+                                <span className="text-danger">Contact List</span>
+                                </h1>
                             {contacts.map(contact =>
                                 <Contact key={contact.id} contact={contact}
                                 />)}
                         </React.Fragment>
 
-                    )
+                    );
                 }
 
                 }
